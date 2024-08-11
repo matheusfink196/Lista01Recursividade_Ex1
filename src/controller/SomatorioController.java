2f15199efe725dee028e548aa192a670d1d5d8dd
matheusfink196 <matheus.fink@fatec.sp.gov.br>
2024-08-11 19:47:01 -0300

    Update SomatorioController.java

--- src/controller/SomatorioController.java
+++ src/controller/SomatorioController.java
@@ -5,10 +5,12 @@ public class SomatorioController {
 		super();
 	}
 	public int somatorio( int n) {
-		if (n <= 0) {
-            return 0;
+	    	if (n <= 0) {
+            		return 0;
+//se valor menor ou igual a zero retorna = ponto de parada
 		} else {
 			return n + somatorio(n - 1);
+//se o n-1 continua a recursiva
 		}
        
 	}
